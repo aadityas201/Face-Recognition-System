d19248311229962e4213b651f85cf65260ba0088
prajwalahetti <parjwal.ahetti@gmail.com>
2022-03-11 00:06:32 +0530

    mail service is on

--- main_video.py
+++ main_video.py
@@ -1,11 +1,12 @@
 import cv2
 from simple_facerec import SimpleFacerec
 import os
-
+import smtplib
+import imghdr
+from email.message import EmailMessage
 # Encode faces from a folder
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
-
 # Load Camera
 cap = cv2.VideoCapture(0)
 temp_name="Unknown"
@@ -39,12 +40,28 @@ while (x!=0 or x!=1): # triggering to take photos
     
         if(temp_name =="Unknown"):
             cv2.imwrite(os.path.join(path,f"Unknown{number}.png"),temp_image)
+            Sender_Email = "" 
+            Reciever_Email = ""
+            Password = "" #type your password here
+            newMessage = EmailMessage()
+            newMessage['Subject'] = "Alert Theft inside your home" 
+            newMessage['From'] = Sender_Email                   
+            newMessage['To'] = Reciever_Email                   
+            newMessage.set_content('Let me know what you think. Image attached!') 
+            with open(os.path.join(path,f"Unknown{number}.png"), 'rb') as f:
+                image_data = f.read()
+                image_type = imghdr.what(f.name)
+                image_name = f.name        
+            newMessage.add_attachment(image_data, maintype='image', subtype=image_type, filename=image_name)
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(Sender_Email, Password)              
+                smtp.send_message(newMessage)        
             number=number+1   
-        x=0     
+        x=0
     elif(x==0):
         print("On Standby")
-            
+cap.release()
+cv2.destroyAllWindows   
         
 
-cap.release()
-cv2.destroyAllWindows
+
