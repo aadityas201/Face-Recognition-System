c1b79a1474727b122f7d66609f566af284f422c6
prajwalahetti <parjwal.ahetti@gmail.com>
2022-03-22 11:40:13 +0530

    pir triggering

--- pir_sensor.py
+++ pir_sensor.py
@@ -3,12 +3,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIR,GPIO.IN)     
 GPIO_PIR = 11
 def triggering ():
-    while True:
-        Current_State = GPIO.input(GPIO_PIR)
-        if(Current_State):
-            return Current_State
-        if(Current_State):
-            time.sleep(0.01)
-            Current_State=0
-        else:
-            return 0
+    Current_State = GPIO.input(GPIO_PIR)
+    if(Current_State):
+        Current_State=0
+        return 1
+    else:
+        return 0
